1b1f9d736f5b91ae46e1d5ae57c17e90e5f5f4ed
Sofia Berlin <sofiamberlin@gmail.com>
2023-03-29 21:20:06 -0700

    get table data as arrays

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
     userDataDir: "./data",
   });
   const page = await browser.newPage();
@@ -14,13 +13,33 @@ import puppeteer from 'puppeteer';
     {waitUntil: "networkidle2"},
     );
 
+  await page.waitForSelector('tr');
+
   // Set screen size
   await page.setViewport({width: 1080, height: 1024});
 
   // Get page data
-  const rows = await page.$$eval('tbody > tr', elements => {
-    return elements.map(el => {el.innerText});
+  // const rows = await page.$$eval('tbody > tr', elements => {
+  //   return elements.map(el => {el.innerText});
+  // });
+
+  const header = await page.$$eval('th', cols => {
+    return Array.from(cols, col => {
+      return col.innerText;
+    });
   });
+
+  console.log(header);
+
+
+  const result = await page.$$eval('tbody tr', rows => {
+    return Array.from(rows, row => {
+      const columns = row.querySelectorAll('td');
+      return Array.from(columns, column => column.innerText);
+    });
+  });
+
+  // console.log(result[4]);
 
   await browser.close();
 })();
